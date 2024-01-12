fc0f0cc29f1f2c6f05da6f3c118630fa4c180de9
sanchezj <53372645+Vredeza@users.noreply.github.com>
2024-01-12 17:53:47 +0100

    Update script_portailConnexion.js

--- web-portal/model/keycloak/scriptsParPage/script_portailConnexion.js
+++ web-portal/model/keycloak/scriptsParPage/script_portailConnexion.js
@@ -6,9 +6,9 @@ window.addEventListener('load', () => {
         if (authenticated) {
             //Gestion de l'utilisateur authentifié
             console.log(serverIp); // Pour vérifier la valeur de serverIp
-            console.log(`http://${serverIp}:8080/realms/CATS/protocol/openid-connect/auth?client_id=portal-cats&redirect_uri=http%3A%2F%2F${serverIp}%3A8888%2Fview%2FUtilisateurs%2FespacePersonnelUtilisateur.html&response_mode=fragment&response_type=code&scope=openid&nonce=e8f2162f-2d69-4ed5-b1fa-8931db645871`);
+            console.log(`http://${serverIp}/keycloak/realms/CATS/protocol/openid-connect/auth?client_id=portal-cats&redirect_uri=http%3A%2F%2F${serverIp}%2Fview%2FUtilisateurs%2FespacePersonnelUtilisateur.html&response_mode=fragment&response_type=code&scope=openid&nonce=e8f2162f-2d69-4ed5-b1fa-8931db645871`);
 
-            window.location.replace(`http://${serverIp}:8080/realms/CATS/protocol/openid-connect/auth?client_id=portal-cats&redirect_uri=http%3A%2F%2F${serverIp}%3A8888%2Fview%2FUtilisateurs%2FespacePersonnelUtilisateur.html&response_mode=fragment&response_type=code&scope=openid&nonce=e8f2162f-2d69-4ed5-b1fa-8931db645871`);
+            window.location.replace(`http://${serverIp}/keycloak/realms/CATS/protocol/openid-connect/auth?client_id=portal-cats&redirect_uri=http%3A%2F%2F${serverIp}%2Fview%2FUtilisateurs%2FespacePersonnelUtilisateur.html&response_mode=fragment&response_type=code&scope=openid&nonce=e8f2162f-2d69-4ed5-b1fa-8931db645871`);
         } else {
             loginBtn.style.display = 'block';
             logoutBtn.style.display = 'none';
