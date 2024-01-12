c5083f5c0896cf2e153cb8afd159d9a918d13ac3
sanchezj <53372645+Vredeza@users.noreply.github.com>
2024-01-12 17:54:12 +0100

    Update keycloak-config.js

--- web-portal/model/keycloak/keycloak-config.js
+++ web-portal/model/keycloak/keycloak-config.js
@@ -1,6 +1,6 @@
 
 const keycloakConfig = {
-    url: `http://${serverIp}:8080/`, // URL de votre serveur keycloak
+    url: `http://${serverIp}/keycloak/`, // URL de votre serveur keycloak
     realm: 'CATS', // Le nom de votre realm
     clientId: 'portal-cats', // L'ID de votre client keycloak
 };
